43d739d6be36b4ac74116184486dcf43dac06a82
Joshua Teves <joshua.teves@nih.gov>
2022-03-10 18:36:24 -0500

    Adds auto-open option

--- pyniivue.py
+++ pyniivue.py
@@ -33,6 +33,10 @@ def main():
         '--port', type=int, default=8888,
         help='Port to try to use. Default 8888.'
     )
+    parser.add_argument(
+        '-n', '--no-open', action='store_true', dest='no_open',
+        help='Do not automatically open the link'
+    )
     args = parser.parse_args()
     host = 'localhost'
     port = int(args.port)
@@ -49,7 +53,8 @@ def main():
         # construct the URL to print in the console. The user will navigate to this URL in their browser
         url = f"http://{host}:{port}/?host={host}&port={port}&files={':'.join(in_files_abs)}"
         print('COPY THIS URL TO YOUR BROWSER: ', url)
-        webbrowser.open(url)
+        if not args.no_open:
+            webbrowser.open(url)
         app.run(host, port, debug=False)
         sys.exit(0)
 

2c0ed023cf1449ea17684fc13c49a70cc3ef971d
Joshua Teves <joshua.teves@nih.gov>
2022-03-11 14:26:16 -0500

    Adds header generation

--- pyniivue.py
+++ pyniivue.py
@@ -24,6 +24,108 @@ def files():
     file = request.args.get('filename')
     return send_file(file, as_attachment=True)
 
+def head(title="basic multiplanar", initial_scale=1.0, margin=20):
+    """Generates a page header
+
+    Parameters
+    ----------
+    title: str
+        The title of the tab. Default "basic multiplanar".
+    initial_scale: float
+        The initial scaling. Default 1.0.
+    margin: int
+        The margin size in pixels. Default 20.
+
+    Returns
+    -------
+    head, a str representing the header of the document
+    """
+    # Relevant substrings to replace are
+    # - INITIAL_SCALE
+    # - TITLE
+    # - MARGIN
+    headstr =  """
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=INITIAL_SCALE">
+    <title>TITLE</title>
+    <style>
+      section {
+        margin: MARGINpx;
+      }
+    </style>
+  </head>
+    """
+    headstr = headstr.replace("INITIAL_SCALE", str(initial_scale))
+    headstr = headstr.replace("TITLE", title)
+    headstr = headstr.replace("MARGIN", str(margin))
+    print(headstr)
+    return headstr[1:-5]
+
+
+# The virtual representation of the HTML page
+def index():
+    """Returns the virtual HTML page
+
+    Returns
+    -------
+    page: str
+        A string representing the HTML index page
+    """
+    page = """
+<!DOCTYPE html>
+<html lang="en">
+"""
+    page += head()
+    page += """
+  <body style="font-family: sans-serif;">
+    <noscript>
+      <strong>niivue doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
+    </noscript>
+
+    <section>
+      <h1>
+        NiiVue
+      </h1>
+    </section>
+
+    <section>
+      <div id="demo" style="width:90%; height:400px;">
+        <canvas id="gl" height=480 width=640>
+        </canvas>
+      </div>
+    </section>
+    <script src="https://unpkg.com/@niivue/niivue@0.22.2/dist/niivue.umd.js">
+    </script>
+    <script>
+
+                    let urlParams = new URLSearchParams(window.location.search)
+            let files = urlParams.get('files')
+                    let host = urlParams.get('host')
+                    let port = urlParams.get('port')
+                    let inFiles = files.split(':')
+                    let volumeList = []
+                    let colors = ['red', 'green', 'blue']
+                    for (i=0; i<inFiles.length; i++) {
+                            volumeList.push({
+                                    url: `http://${host}:${port}/files?filename=${inFiles[i]}`,
+                                    colorMap: i < 1 ? 'gray' : colors[Math.floor(Math.random() * (3 - 0) + 0)]
+                            })
+                    }
+      const nv = new niivue.Niivue()
+      nv.attachTo('gl')
+      nv.loadVolumes(volumeList)
+      nv.setSliceType(nv.sliceTypeMultiplanar)
+
+    </script>
+  </body>
+</html>
+    """
+    # Trim off extra characters
+    return page[1:-4] + '\n'
+
+
 def main():
     parser = ArgumentParser(description='app for viewing nifti images')
     # Required
@@ -42,6 +144,10 @@ def main():
     port = int(args.port)
     in_files = args.files
     in_files_abs = [os.path.abspath(f) for f in in_files] # make sure we have the absolute path for each file
+
+    # Write the index file
+    with open('index.html', 'w') as f:
+        f.write(index())
 
     while True:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
